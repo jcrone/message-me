389370aeeb82a8321b381399ba66fda45506d35b
Jamie Crone <jamiecrone@gmail.com>
2020-06-12 09:24:36 -0700

    Added User controls, Login logout. Set paramaters for pages. added flash messages. added flash js

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(username: "Daniel", password: "jamie4cro")
+User.create(username: "Rachel", password: "jamie4cro")
+User.create(username: "Kevin", password: "jamie4cro")
+User.create(username: "James", password: "jamie4cro")
